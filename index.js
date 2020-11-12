617fd382f170cbcfd9620e1cd04dbdf2cb42ea08
DavidPinedaHidalgo <davidpinedahidalgo@gmail.com>
2020-11-12 12:08:23 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // CONTENIDO DE LA PRÁCTICA:
-// Vamos a añadir elementos en una lista (con la clase "color-list") con javascript a partir del array aportado en este documento, en la constante "colorList" (ver imagen en el proyecto "ejemplo_lista.png"). 
+// Vamos a añadir elementos en una lista (con la clase "color-list") con javascript a partir del array aportado en este documento, en la constante "colorList" (ver imagen en el proyecto "ejemplo_lista.png").
 
 // Como se puede apreciar en la imagen, cada elemento que esté en una posición par de de la lista tiene que tener la clase "color-item--odd". Esta clase debe añadirse desde javascript, NO haciendo uso del selector css nth-of-type(odd) o similares. NOTA: En este caso vamos a considerar un elemento par pensando en el primer elemento como el 1 no como el 0.
 
@@ -28,35 +28,104 @@
 
 const colorList = [
   {
-    colorName: 'white',
-    hex: '#ffffff'
+    colorName: "white",
+    hex: "#ffffff"
   },
   {
-    colorName: 'red',
-    hex: '#ff0000'
+    colorName: "red",
+    hex: "#ff0000"
   },
   {
-    colorName: 'orange',
-    hex: '#ffa500'
+    colorName: "orange",
+    hex: "#ffa500"
   },
   {
-    colorName: 'yellow',
-    hex: '#ffff00'
+    colorName: "yellow",
+    hex: "#ffff00"
   },
   {
-    colorName: 'orchid',
-    hex: '#da70d6'
+    colorName: "orchid",
+    hex: "#da70d6"
   },
   {
-    colorName: 'pink',
-    hex: '#ffc0cb'
+    colorName: "pink",
+    hex: "#ffc0cb"
   },
   {
-    colorName: 'green',
-    hex: '#008000'
+    colorName: "green",
+    hex: "#008000"
   },
   {
-    colorName: 'silver',
-    hex: '#c0c0c0'
+    colorName: "silver",
+    hex: "#c0c0c0"
   }
 ];
+
+const list = document.querySelector(".color-list");
+
+colorList.forEach(crearList);
+
+function crearList(colorElement, i) {
+  //Creacion de elementos
+  const item = document.createElement("li");
+  const nombre = document.createElement("div");
+  const div = document.createElement("div");
+  const next = document.createElement("button");
+  const backgroundColor = document.createElement("button");
+
+  //Añade elementos a la lista
+  item.appendChild(nombre);
+  item.appendChild(div);
+  item.appendChild(next);
+  item.appendChild(backgroundColor);
+  list.appendChild(item);
+
+  //Añade clases
+  item.classList.add("color-item");
+  nombre.classList.add("color-name");
+  div.classList.add("color-show");
+  next.classList.add("color-set");
+  backgroundColor.classList.add("color-set");
+  if (i % 2 !== 0) {
+    item.classList.add("color-item--odd");
+  }
+
+  //Añade textos
+  nombre.textContent = "Color: " + colorElement.colorName;
+  div.textContent = "Muestra";
+  next.textContent = "Next item color";
+  backgroundColor.textContent = "Page color";
+
+  //Añade color de muestra
+  div.style.backgroundColor = colorElement.hex;
+
+  //Saca alerta con el nombre del color
+  item.addEventListener("click", e => {
+    e.stopPropagation();
+    alert(colorElement.colorName);
+  });
+
+  // Cambia color del siguiente elemento
+  next.addEventListener("click", nextElementColor, false);
+  function nextElementColor(evento) {
+    evento.stopPropagation();
+    if (colorList.length - 1 !== i) {
+      next.parentNode.nextSibling.style.backgroundColor = colorElement.hex;
+    } else {
+      next.parentNode.parentNode.children[1].style.backgroundColor =
+        colorElement.hex;
+    }
+  }
+
+  // Cambia color del fondo de la pagina
+  backgroundColor.addEventListener("click", backColor, false);
+  function backColor(evento) {
+    evento.stopPropagation();
+    document.body.style.backgroundColor = colorElement.hex;
+  }
+}
+
+//Evento click en el fondo
+document.body.addEventListener("click", () => {
+  alert("body");
+});
